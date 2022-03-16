80baa2a54f5fe36dc9241b2a4b84397e81811fb0
santi <santi.cacciola@hotmail.it>
2022-03-16 19:14:36 +0000

    update

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,29 +1,34 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function ApiFunctions() {
-  const [pokemon, setPokemon] = useState("");
+  const [pokemon, setPokemon] = useState({});
 
   function randomNumber() {
-    return Math.floor(Math.random() * 897) + 1;
+    return Math.floor(Math.random() * 897) + 1; //TOTAL POKEMON: 898
   }
+  async function fetchPokemon() {
+    let pokemons = [];
+    for (let i = 0; i < 5; i++) {
+      let random = randomNumber();
+      let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`);  
+      let pokemon = await response.json();
+      pokemons.push({ 
+          id:pokemon.id, 
+          name: pokemon.name, 
+          url: pokemon.sprites.front_default
+         });
+    }
+    return pokemons;
+  }
+    fetchPokemon().then((data)=>{setPokemon(data)})
 
-  const pokemonAPI = async function fetchPokemon() {
-    const random = randomNumber();
-    const url = `https://pokeapi.co/api/v2/pokemon/${random}`; //898
-    const response = await fetch(url);
-    return await response.json();
-  };
-
-  useEffect(() => {
-    pokemonAPI().then((data) => {
-      setPokemon(data.sprites.front_default);
-      console.log(data);
-    });
-  }, []);
 
   return (
     <>
-      <img src={pokemon} alt="pokemon"></img>
+      <img src={pokemon[0].url}></img>
+      <div>
+        {pokemon[0].id} . {pokemon[0].name}
+      </div>
     </>
   );
 }
